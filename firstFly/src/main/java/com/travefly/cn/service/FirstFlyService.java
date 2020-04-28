a856737dc784cc29082c4795347e9c21032def3e
wdm <935630781@qq.com>
2020-04-28 14:43:36 +0800

    分支开发，先更新，更新后可能会有大量冲突，因为是前一个分支遗留下的，可能你之际需要解决冲突，有一个问题就是，，单个分支多进行pull,因为中途可能有其他分支进行了合并，那么你的代码就必须要更新，因为你来自于分支，没有写好之前尽量不要合并，每天pull 分支和 pull master ,提交后需要切换分支，合并分支到master,然后关闭本地仓库分支，关闭远程仓库分支

--- firstFly/src/main/java/com/travefly/cn/service/FirstFlyService.java
+++ firstFly/src/main/java/com/travefly/cn/service/FirstFlyService.java
@@ -3,6 +3,7 @@ package com.travefly.cn.service;
 public class FirstFlyService {
     public double sum(double a,double b){
         double result = 0d;
+        resut = a + b;
         return result;
     }
 }
